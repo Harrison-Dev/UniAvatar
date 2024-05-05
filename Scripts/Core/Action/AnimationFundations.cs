f4d9665f13bbc20102f9d8001aab845547d28f02
Harrison-Dev <hao0628@gmail.com>
2024-05-05 14:36:10 +0800

    rename stuff

---
+++ Scripts/Core/Action/AnimationFundations.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+namespace UniAvatar
+{
+    public abstract class AnimationTargetBase : MonoBehaviour
+    {
+
+    }
+
+    public abstract class AnimationFunctionBase : ScriptableObject
+    {
+        public AnimationFunctionBase CreateInstance()
+        {
+            return Instantiate(this);
+        }
+
+        public abstract void Interrupt();
+        public abstract void Play(AnimationTargetBase target);
+    }
+}
---
+++ Scripts/Core/Action/IAnimatable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace UniAvatar
+{
+    interface IAnimatable { void Interrupt(); }
+
+    interface IFlip : IAnimatable
+    {
+        void Flip();
+    }
+
+    interface IFade : IAnimatable
+    {
+        void Fade(float alpha, float time);
+    }
+
+    interface IPan : IAnimatable
+    {
+        void Pan(float localValue, float time);
+    }
+
+    interface ITint : IAnimatable
+    {
+        void Tint(Color tintTarget, float time);
+    }
+
+    interface IJump : IAnimatable
+    {
+        void Jump();
+    }
+
+    interface ISpriteChange : IAnimatable
+    {
+        void Change(Sprite sprite);
+    }
+
+}
